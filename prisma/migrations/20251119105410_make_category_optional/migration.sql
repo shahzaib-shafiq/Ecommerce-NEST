9c1819ac694d4e7b551380a94dce39625037d6fd
shahzaib-busona <73356944+shahzaib-shafiq@users.noreply.github.com>
2025-11-19 16:16:57 +0500

    Added Category table

---
+++ prisma/migrations/20251119105410_make_category_optional/migration.sql
@@ -0,0 +1,16 @@
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "categoryId" UUID;
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
