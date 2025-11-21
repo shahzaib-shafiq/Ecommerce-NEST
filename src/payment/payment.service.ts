da194e7db3c01e588e070262f3358193cff7ab68
shahzaib-busona <73356944+shahzaib-shafiq@users.noreply.github.com>
2025-11-21 12:19:17 +0500

    Updated Create Payment Function

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException ,BadGatewayException,BadRequestException} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
 import { CreatePaymentDto } from './dto/create-payment.dto';
@@ -8,15 +8,42 @@ export class PaymentService {
   constructor(private prisma: PrismaService) {}
 
   async create(dto: CreatePaymentDto) {
+    // 1. Check if order exists
+    const order = await this.prisma.order.findUnique({
+      where: { id: dto.orderId },
+    });
+  
+    if (!order) {
+      throw new NotFoundException('Order not found');
+    }
+  
+    // 2. Ensure only 1 payment per order
+    const existingPayment = await this.prisma.payment.findFirst({
+      where: { orderId: dto.orderId },
+    });
+  
+    if (existingPayment) {
+      throw new BadRequestException('Payment already exists for this order');
+    }
+  
+    // 3. Override amount using the order.total
+    const finalAmount = order.total;
+  
     return this.prisma.payment.create({
       data: {
-        ...dto,
+        orderId: dto.orderId,
+        amount: finalAmount,
+        method: dto.method,
+        status: dto.status ?? 'PENDING',
+        transactionId: dto.transactionId,
+        metadata: dto.metadata,
       },
       include: {
-        order: true, // join
+        order: true,
       },
     });
   }
+  
 
   async findAll() {
     return this.prisma.payment.findMany({
