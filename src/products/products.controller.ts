37af3194da341c46ccb33be2bb18e7a42ccfa20a
shahzaib-busona <73356944+shahzaib-shafiq@users.noreply.github.com>
2025-11-28 18:09:06 +0500

    Added Bulk Product Upload With Excel File

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete ,UseGuards} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete ,UseGuards,BadRequestException,UseInterceptors} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-
-
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadedFile } from '@nestjs/common';
+// import { FileInterceptor } from '@nestjs/platform-express';
+import { multerConfig,excelFileFilter,} from '../upload/multer.config';
 @UseGuards(JwtAuthGuard)
 @Controller('products')
 export class ProductsController {
@@ -33,5 +35,22 @@ export class ProductsController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
+  }
+  @Post('upload-bulk-products')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      ...multerConfig,
+      fileFilter: excelFileFilter,
+      limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB
+    }),
+  )
+  async uploadExcel(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    if (!file) {
+      throw new BadRequestException('Excel file is required');
+    }
+    // return data from file
+    
+    return await this.productsService.importProductsFromExcel(file.path);
   }
 }
